b1caf55e54cf2ab8a44d04391cab6efceae4ca28
CaptainLazarus <adityagudimetla@gmail.com>
2023-08-28 15:47:46 +0530

    Hardcoded Fully qualified list

--- Code/KT Generator/CodeParser.py
+++ Code/KT Generator/CodeParser.py
@@ -1,7 +1,8 @@
 import ast
+from loguru import logger
+
 
 class code_parser:
-
     def extract_classes_from_code(self, code: str):
         parsed_code = ast.parse(code)
         classes = []
@@ -36,44 +37,56 @@ class code_parser:
 
         return output
 
-    def extract_elements(self, source: str):
+    def extract_elements(self, source: str, module_name: str) -> str:
         node = ast.parse(source)
+        imports_block = []
+        output = {}
 
-        def is_at_module_level(n):
-            for parent in ast.walk(n):
-                if isinstance(parent, (ast.FunctionDef, ast.ClassDef, ast.With)):
-                    return False
-            return True
+        # AST helper method to get the source code for a given node
+        def get_source_lines(start, end):
+            return "".join(source.splitlines(True)[start - 1 : end])
 
-        elements = []
-
-        imports_block = ""
-
+        # Iterate through each node in the AST
         for n in ast.walk(node):
+            # Check if the node represents an import statement
             if isinstance(n, (ast.Import, ast.ImportFrom)):
-                if is_at_module_level(n):
-                    start_line = n.lineno - 1
-                    end_line = n.lineno
-                    imports_block += "".join(source.splitlines(True)[start_line:end_line])
-            elif isinstance(n, ast.FunctionDef):
-                if is_at_module_level(n):  # Ensure the function is not within a class
-                    start_line = n.lineno - 1
-                    end_line = max(
-                        (x.lineno for x in ast.walk(n) if hasattr(x, "lineno")), default=n.lineno
-                    )
-                    elements.append("".join(source.splitlines(True)[start_line:end_line]))
-            elif isinstance(n, ast.ClassDef):
+                logger.info("found import")
+                # Extract the start and end line numbers
                 start_line = n.lineno - 1
-                end_line = max(
-                    (x.lineno for x in ast.walk(n) if hasattr(x, "lineno")), default=n.lineno
-                )
-                class_code = "".join(source.splitlines(True)[start_line:end_line])
-                if (end_line - start_line) > 10:
-                    classes_functions = self.extract_classes_from_code(class_code)
-                    elements.extend(classes_functions)
-                else:
-                    elements.append(class_code)
-            # Add more handlers if you need to extract more types of nodes
+                end_line = n.lineno
+
+                # Append the import statement to the imports_block string
+                imports_block.append(source.splitlines(True)[start_line:end_line])
+            if isinstance(n, ast.ClassDef):
+                logger.info("Found Class")
+                class_name = n.name
+                class_start_line = n.lineno
+                class_end_line = n.end_lineno
+
+                # Get the code block for the entire class
+                class_body = get_source_lines(class_start_line, class_end_line)
+
+                # Look for method definitions within the class
+                for sub_node in ast.iter_child_nodes(n):
+                    if isinstance(sub_node, (ast.FunctionDef, ast.AsyncFunctionDef)):
+                        method_name = sub_node.name
+                        method_start_line = sub_node.lineno
+                        method_end_line = sub_node.end_lineno
+
+                        # Get the code block for the method
+                        method_body = get_source_lines(
+                            method_start_line, method_end_line
+                        )
+
+                        output[class_name + "." + method_name] = {
+                            "body" : method_body,
+                            "class_name" : class_name
+                        }
+                output[class_name] = {
+                    "body": class_body
+                }
+
+        return output
 
         # Prepend the imports block if there were any imports
         if imports_block:

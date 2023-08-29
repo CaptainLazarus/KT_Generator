61a9dbfc508760c596d2f8ad4819099a48574b72
Sidu Ponnappa <ckponnappa@gmail.com>
2023-08-29 09:57:43 +0800

    [Sidu] - Remove unused method CodeParser#extract_classes_from_code

--- src/kt_generator/CodeParser.py
+++ src/kt_generator/CodeParser.py
@@ -3,40 +3,6 @@ from loguru import logger
 
 
 class CodeParser:
-    def extract_classes_from_code(self, code: str):
-        parsed_code = ast.parse(code)
-        classes = []
-        for node in parsed_code.body:
-            if isinstance(node, ast.ClassDef):
-                class_info = {
-                    "class_name": node.name,
-                    "docstring": ast.get_docstring(node),
-                    "init_method": None,
-                    "methods": [],
-                }
-
-                for class_node in node.body:
-                    if isinstance(class_node, ast.FunctionDef):
-                        if class_node.name == "__init__":
-                            init_method_code = ast.get_source_segment(code, class_node)
-                            class_info["init_method"] = init_method_code
-                        else:
-                            method_code = ast.get_source_segment(code, class_node)
-                            class_info["methods"].append(method_code)
-                classes.append(class_info)
-
-        output = []
-        for class_info in classes:
-            class_info["docstring"] = (
-                "" if class_info["docstring"] is None else class_info["docstring"]
-            )
-            class_information = f"Class {class_info['class_name']} \n {class_info['docstring']} \n {class_info['init_method']}"
-            output.append(class_information)
-            for method in class_info["methods"]:
-                output.append(method)
-
-        return output
-
     def extract_elements(self, source: str, module_name: str) -> str:
         node = ast.parse(source)
         imports_block = []
